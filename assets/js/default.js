96162afa9db8d91b3dc3bb1c367bff46633f5994
Sudipta Biswas <sudipta.biswas.5648@gmail.com>
2024-11-05 01:10:23 +0530

    Initial commit

---
+++ assets/js/default.js
@@ -0,0 +1,14 @@
+// Testimonial
+$('#testi_slider').owlCarousel({
+    margin:0,
+    items: 1,
+    nav:true,
+    navText : ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"], 
+    dots:false,
+    loop:true,
+    smartSpeed:2000,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true,
+    responsiveClass:true, 
+});
